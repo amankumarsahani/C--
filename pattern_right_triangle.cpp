f5239fa33d9d867acf71cd8987c7bfab41baf582
Aman Kumar Sahani <71867895+amankumarsahani@users.noreply.github.com>
2022-11-04 19:42:07 +0530

    Pattern Right Triangle

---
+++ pattern_right_triangle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Size of pattern : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==j || i<j){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
