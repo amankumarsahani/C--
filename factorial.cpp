ff830bc6de08e7bcb2021d8227237a8c68363109
Aman Kumar Sahani <71867895+amankumarsahani@users.noreply.github.com>
2022-11-06 23:54:55 +0530

    Factorial of a Number

---
+++ factorial.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,fact=1;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        fact*=i;
+    }
+    cout<<"Factorial of "<<n<<" is : "<<fact;
+    return 0;
+}
